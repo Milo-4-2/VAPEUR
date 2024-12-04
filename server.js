3550eef3b141863f950b07a838692bcc488a4d86
Milo-4-2 <milo.vidal402@gmail.com>
2024-12-04 19:14:57 +0100

    HBS file configuration

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const path = require("path"); // Import the path module
 const app = express();
+app.disable("view cache");
 //const { PrismaClient } = require("@prisma/client");
 const bodyParser = require("body-parser");
 //const prisma = new PrismaClient();
 const hbs = require("hbs");
 //console.log("HBS loaded:", hbs);
 app.set("view engine", "hbs"); // On définit le moteur de template que Express va utiliser
-app.engine('html', require('hbs').__express);
 app.set("views", path.join(__dirname, "views")); // On définit le dossier des vues (dans lequel se trouvent les fichiers .hbs)
 hbs.registerPartials(path.join(__dirname, "views", "partials")); // On définit le dossier des partials (composants e.g. header, footer, menu...)
+app.engine('hbs', require('hbs').__express);
 const PORT = 3000;
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     // on passe seulement le nom du fichier .hbs sans l'extention.
     // Le chemin est relatif au dossier `views`.
     // On peut aller chercher des templates dans les sous-dossiers (e.g. `movies/details`).
     res.render("index");
+    //res.sendFile(path.join(__dirname, "views", "index.hbs"));
     //res.send("Hello, World!");
 });
 
