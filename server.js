e2b251405f2cb6147ef2c3f60e5463be07f8df1b
Milo-4-2 <milo.vidal402@gmail.com>
2024-12-04 14:19:41 +0100

    HBS configuration

--- server.js
+++ server.js
@@ -4,4 +4,7 @@ const { PrismaClient } = require("@prisma/client");
 const bodyParser = require("body-parser");
 const prisma = new PrismaClient();
 const hbs = require("hbs");
+app.set("view engine", "hbs"); // On définit le moteur de template que Express va utiliser
+app.set("views", path.join(__dirname, "views")); // On définit le dossier des vues (dans lequel se trouvent les fichiers .hbs)
+hbs.registerPartials(path.join(__dirname, "views", "partials")); // On définit le dossier des partials (composants e.g. header, footer, menu...)
 const PORT = 3000;
